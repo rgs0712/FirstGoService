b903dba36b87559849fb344ab0b94dcf3c1d865b
Rafael <rgarciasantanna@gmail.com>
2020-01-20 22:41:33 -0300

    adicionando métodos delete e get

--- FirstGoService/entities/Person.go
+++ FirstGoService/entities/Person.go
@@ -1,11 +1,14 @@
 package entities
 
+var Index int = 0
+
 type Person struct {
 	Id        int
 	FirstName string
 	LastName  string
 }
 
-func NewPerson(Id int, FirstName string, LastName string) Person {
-	return Person{Id: Id, FirstName: FirstName, LastName: LastName}
+func NewPerson(FirstName string, LastName string) Person {
+	Index++
+	return Person{Id: Index, FirstName: FirstName, LastName: LastName}
 }
--- FirstGoService/main.go
+++ FirstGoService/main.go
@@ -4,19 +4,71 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
+	"strconv"
 
 	"./entities"
+	"github.com/gorilla/mux"
 )
 
-var peaple []entities.Person
+var ListPerson []entities.Person
 
 func main() {
-	peaple = append(peaple, entities.NewPerson(1, "Rafael", "Santanna"))
-	peaple = append(peaple, entities.NewPerson(2, "Tatiane", "Alboreli"))
+	ListPerson = append(ListPerson, entities.NewPerson("Rafael", "Santanna"))
+	ListPerson = append(ListPerson, entities.NewPerson("Tatiane", "Alboreli"))
 
-	resp, err := json.Marshal(peaple)
-	if err != nil {
-		log.Fatal("Cannot encode to JSON ", err)
+	router := mux.NewRouter()
+	router.HandleFunc("/contato", List).Methods("GET")
+	router.HandleFunc("/contato/{id}", Get).Methods("GET")
+	router.HandleFunc("/contato", Create).Methods("POST")
+	router.HandleFunc("/contato/{id}", Delete).Methods("DELETE")
+	log.Fatal(http.ListenAndServe(":8000", router))
+}
+
+func List(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(ListPerson)
+}
+
+func Get(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+
+	for _, item := range ListPerson {
+		Id, error := strconv.ParseInt(params["id"], 10, 64)
+
+		if error == nil {
+			fmt.Printf("%d of type %T", Id, Id)
+		}
+		if item.Id == int(Id) {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
 	}
-	fmt.Printf(string(resp))
+
+	w.WriteHeader(500)
+	json.NewEncoder(w).Encode("")
+}
+
+func Delete(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	for index, item := range ListPerson {
+		Id, error := strconv.ParseInt(params["id"], 10, 64)
+
+		if error == nil {
+			fmt.Printf("%d of type %T", Id, Id)
+		}
+
+		if item.Id == int(Id) {
+			ListPerson = append(ListPerson[:index], ListPerson[index+1:]...)
+			break
+		}
+		json.NewEncoder(w)
+	}
+}
+
+func Create(w http.ResponseWriter, r *http.Request) {
+	var person entities.Person
+	_ = json.NewDecoder(r.Body).Decode(&person)
+
+	ListPerson = append(ListPerson, person)
+	json.NewEncoder(w).Encode(person)
 }
